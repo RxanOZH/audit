efdd8b53b57f8abbc481754cddfa047d38acb3a3
Ryan <Ryanziheng@gmail.com>
2026-09-04 10:47:41 +0800

    activity3

--- hello.py
+++ hello.py
@@ -10,3 +10,12 @@ followers= 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+followers += 50
+print("Day1", followers)
+
+followers += 20 
+print("Day2", followers)
+
+followers -= 10
+print("Day3", followers) 
