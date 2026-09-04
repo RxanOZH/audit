9e1294cbef3ad2b0e58ff9c8686951313ec14516
Ryan <Ryanziheng@gmail.com>
2026-09-04 10:42:04 +0800

    activity 2

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==========")
 print("welcome here")
 print("My First Post")
 print("==========")
+
+username = "cool_creator"
+bio="Fun Blogger"
+followers= 100 
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
