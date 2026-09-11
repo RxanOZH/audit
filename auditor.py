37848e2193e7ee39a8e8d978b8a66738bb897373
Ryan <2602955@sit.singaporetech.edu.sg>
2026-09-11 11:49:22 +0800

    lab2

--- auditor.py
+++ auditor.py
@@ -1,5 +1,30 @@
-inventory = 0
-print("======================")
-print("Enter Stock quantity:")
-inventory = int(input())
-print("Current Stock:", inventory)
+print("===================")
+print("smart inventory system")
+print("===================")
+inventory = 0 
+failed_input =0 
+
+
+while True:
+    user_input = input("enter inventory quantity:")
+    if user_input.isdigit():
+        user_int =int(user_input)
+        inventory += user_int
+        if inventory > 500:
+            print("Alert: Inventory overflow")
+            break
+    
+    elif user_input.lower() == "quit":
+        print(f"Total inventory: {inventory}")
+        print(f"Failed input attempts: {failed_input}")
+        break
+
+    else:
+        failed_input += 1
+        print("Invalid input. Please enter a valid number.")
+
+
+
+
+
+                
