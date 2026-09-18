f12bec73f2b259ccca32c764057b1b4d1ae136de
Ryan <2602955@sit.singaporetech.edu.sg>
2026-09-18 09:51:21 +0800

    start

---
+++ modular_auditor.py
@@ -0,0 +1,24 @@
+print("===================")
+print("smart inventory system")
+print("===================")
+inventory = 0 
+failed_input =0 
+
+
+while True:
+    user_input = input("enter inventory quantity:")
+    if user_input.isdigit():
+        user_int =int(user_input)
+        inventory += user_int
+        if inventory > 500:
+            print("Alert: Inventory overflow")
+            break
+    
+    elif user_input.lower() == "quit":
+        print(f"Total inventory: {inventory}")
+        print(f"Failed input attempts: {failed_input}")
+        break
+
+    else:
+        failed_input += 1
+        print("Invalid input. Please enter a valid number.")
