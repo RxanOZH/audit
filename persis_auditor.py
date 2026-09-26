9517c3ed898b0cad9d2b485da7e3e1ef8987f069
Ryan <2602955@sit.singaporetech.edu.sg>
2026-09-26 21:58:56 +0800

    save txt

--- persis_auditor.py
+++ persis_auditor.py
@@ -1,10 +1,7 @@
 print("===================")
 print("smart inventory system")
 print("===================")
-inventory = 0 
-failed_input =0 
-delivery = 0 
-order_number = 1
+order_number =1001
 list1 = []
 new_list = []
 
@@ -16,40 +13,55 @@ def get_valid_input(user_input):
     else:
         return None
 
-def generator_report(inventory, failed_input):
-    print(f"Total inventory: {inventory}")
-    print(f"Failed input attempts: {failed_input}")
+def get_product_name(user_input):
+    if user_input.lower() == "quit":
+        return "quit"
+    elif user_input.isdigit():
+        print("Invalid input. Please enter a valid product name.")
+    elif user_input == "":
+       return None 
+    else:
+        return user_input
     
-def save_invetory(user_input):
+def save_inventory(list1):
     with open("inventory.txt", "a") as file:
-        file.write(f"{user_input}")
+        file.write(f"{list1}" + "\n")
     
-
-
 
     
 while True:
-    
+    list1.clear()
     user_input = input("enter product name:")
-    if user_input.lower() == "quit":
+    user_input=get_product_name(user_input)
+    if user_input == "quit":
+        print("this program quit")
         break
     else:
-        list1.append(order_number)
-        save_invetory(order_number)
-        order_number += 1
-        save_invetory(user_input)
-        list1.append(user_input)
-        user_input = input("enter inventory quantity:")
+        product_name = user_input
+        user_input = input("enter Quantity:")
         user_input = get_valid_input(user_input)
-        save_invetory(user_input)
         if user_input is not None:
             if user_input == "quit":
-                break
+             print("this program quit")
+             break
             else:
-                list1.append(user_input)
+                product_quantity = user_input
+                list1.append([order_number,product_name,product_quantity])
+                save_inventory(list1)
+                order_number +=1
+                
+        else:
+            print("invalid input.PLease enter valid number")
+                
+        
+
+    
+    
+
+        
 
 
-    print (list1)
+    
 
     
  
